b35410b63755ae8756f6a615569cbc57b49557a4
Léo Perrio <leoperrio@live.fr>
2022-07-29 02:06:11 +0200

    Création de tuiles personnalisés et plus diversifiées. Complexification du monde. Ajout d'une musique de font. Ajout d'un fondu en ouverture.

--- conf.lua
+++ conf.lua
@@ -1,6 +1,6 @@
 function love.conf(t)
     t.window.width = 1920
     t.window.height = 1080
-    t.window.title = "Test"
+    t.window.title = "TESTORAMA"
     t.console = true
 end
--- main.lua
+++ main.lua
@@ -5,6 +5,18 @@ Player = require "player"
 HudTest = require "hud-test"
 
 function love.load()
+
+  --FadeIn
+  fadeInDuration = 2.2
+  fadeInChrono = 0
+  fadeInComplete = false
+
+  -- Theme
+  islandTheme = love.audio.newSource("resources/audio/island.mp3", "stream")
+  islandTheme:setLooping(true)
+  islandTheme:setVolume(0.3)
+  islandTheme:play()
+
   Map:init()
   HudTest:init()
   Player:init(Map.center_x, Map.center_y) -- On initialise le joueur au centre de la carte
@@ -16,6 +28,13 @@ function love.load()
 end
 
 function love.update(dt)
+
+  if fadeInComplete == false and fadeInChrono < fadeInDuration then
+    fadeInChrono = fadeInChrono + dt
+  elseif fadeInChrono >= fadeInDuration and fadeInComplete == false then
+    fadeInComplete = true
+  end
+
   Player:update(dt)
 end
 
@@ -27,7 +46,16 @@ function love.draw()
   Map:draw()
   --HudTest:draw()
 
-  love.graphics.print("REAL PLAYER POS", 10, 10)
-  love.graphics.print("RealPlayerX:" .. math.floor(Player.window_x) .. " RealPlayerY:" .. math.floor(Player.window_y), 10, 25)
+  love.graphics.print("TESTORAMA", 10, 10)
+  love.graphics.print("Version Alpha 0.1", 10, 25)
+
+  if fadeInComplete == false then
+
+    -- Calcul de l'alpha sur la base de la completion du "FadeIn"
+    local delta = fadeInChrono / fadeInDuration
+
+    love.graphics.setColor(0, 0, 0, 1-delta)
+    love.graphics.rectangle("fill", 0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+  end
 
 end
